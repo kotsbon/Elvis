e25ca16f83180276c68fd18bac5afb9f72b8935c
JW <worladge@icloud.com>
2025-01-27 16:12:43 +1100

    Elvis Costello Challenge 1

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -1,20 +1,66 @@
 //
 //  ContentView.swift
 //  Elvis
-//
+//From Prof.G YouTube Evis Costello Challenge #1
 //  Created by James Worladge on 27/1/2025.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = ""
+    @State private var imageName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .foregroundStyle(.purple)
+                .fontWeight(.black)
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            Text(message)
+                .foregroundStyle(.purple)
+                .fontWeight(.black)
+                .font(.largeTitle)
+        
+        Spacer()
+        
+        HStack {
+            Button("Peace")   {
+                imageName = "peacesign"
+                message = "Peace"
+            }
+            Button("Love")   {
+                imageName = "heart"
+                message = "Love"
+            }
+            Button("Understanding")   {
+                imageName = "lightbulb"
+                message = "Understanding"
+            }
+            
         }
+                    
+            Button("Clear") {
+                imageName = ""
+                message = ""
+                   
+            }
+            
+        
+        
+    }
+        .buttonStyle(.borderedProminent)
+        .tint(.purple)
         .padding()
     }
 }
